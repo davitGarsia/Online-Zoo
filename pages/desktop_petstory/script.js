2e9763588f5994a67fd2df0d1639e7fde6595918
davitGarsia <davitgarsiashviligmail.com>
2022-10-30 19:05:11 +0400

    feature: add rest of the features

---
+++ pages/desktop_petstory/script.js
@@ -0,0 +1,21 @@
+'use strict';
+const nav = document.querySelector('.main-nav-list');
+const about = document.querySelector('.main-nav-link');
+const section1 = document.querySelector('#section--1');
+const logo = document.querySelector('.logo-container');
+const mail = document.querySelector('.gmail');
+
+nav.addEventListener('click', e => {
+  e.preventDefault();
+  about.classList.add('abt');
+});
+
+about.addEventListener('click', function (e) {
+  e.preventDefault();
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+logo.addEventListener('click', function (e) {
+  e.preventDefault();
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
